da0bbe00ac444f8e94c2e3f378dbdcd148bed9d8
Roi <roi200050@gmail.com>
2018-04-09 22:25:21 +0300

    Comment example added

---
+++ Comments Example.js
@@ -0,0 +1,14 @@
+/* Function to add two numbers
+//parameters
+/ x - First number
+/ y - Second number
+// return value
+/ sum - the sum of the two numbers
+*/
+
+function addTwoNumbers(x, y) {
+  let sum = x + y;
+  return sum;
+}
+
+addTwoNumbers(3, 5);
